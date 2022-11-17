67f2d0d5c9559b3c85e7b28849a4585ba9641d93
Stuart Stein <qmstuart@gmail.com>
2022-11-17 00:41:56 +0000

    added keypress to keyboard keys

--- src/main/exfn/events.cljs
+++ src/main/exfn/events.cljs
@@ -11,7 +11,7 @@
                  shuffle
                  first)
     
-    :guesses {1 {1 "E", 2 "A", 3 "R", 4 "T", 5 "H"}
+    :guesses {1 {1 "", 2 "", 3 "", 4 "", 5 ""}
               2 {1 "", 2 "", 3 "", 4 "", 5 ""}
               3 {1 "", 2 "", 3 "", 4 "", 5 ""}
               4 {1 "", 2 "", 3 "", 4 "", 5 ""}
@@ -19,10 +19,27 @@
               6 {1 "", 2 "", 3 "", 4 "", 5 ""}}
     
     :guessed-letters #{}
-    }))
+    :current-row 1
+    :current-col 1}))
+
+(defn inc-max [n]
+  (min (inc n) 6))
+
+(defn dec-min [n]
+  (max (dec n) 1))
 
 (rf/reg-event-db
  :clicked
  (fn [db [_ key]]
-   #_(update db :guessed-letters conj key)
-   db))
+   (condp = key
+     "DEL" (-> db
+               (update :guesses assoc-in [(db :current-row) (dec (db :current-col))] "")
+               (update :current-col dec-min))
+     
+     "ENTER" (-> db)
+     
+     (-> db
+         (update :guesses assoc-in [(db :current-row) (db :current-col)] key)
+         (update :current-col inc-max))
+     
+     )))
